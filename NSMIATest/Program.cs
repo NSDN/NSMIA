c3d95eabaf8e54c2bf07df93f4d070fc00471fc3
drzzm32 <drzzm32@gmail.com>
2020-07-30 09:25:37 +0800

    add host

---
+++ NSMIATest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NSMIALib.HID;
+
+namespace NSMIATest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hid hid = new Hid();
+            IntPtr ptr = new IntPtr();
+            hid.DataReceived += Hid_DataReceived;
+            if ((int)(ptr = hid.OpenDevice(0x3232, 0x0001, 0x02)) != -1)
+            {
+                Console.WriteLine("sending...");
+                Report report = new Report(0x03, new byte[]{ 0x00, 0x55, 0xAA, 0xFF });
+                hid.Write(report);
+            }
+            Console.ReadKey(true);
+        }
+
+        private static void Hid_DataReceived(object sender, Report e)
+        {
+            Console.Write("[" + e.reportID + "] ");
+            Console.WriteLine(e.reportBuff);
+        }
+    }
+}
